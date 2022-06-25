68f30616cc41ccd23c94a60a3b62cae31bbbe3d1
Hattori-Takuma <hattori.ta@icloud.com>
2022-06-25 14:29:00 +0900

    tweet読み取り

--- src/pages/Tweet.tsx
+++ src/pages/Tweet.tsx
@@ -1,7 +1,11 @@
-import React, { useState } from 'react';
-import { TextField } from "@mui/material";
+import React, { useState,useEffect } from 'react';
+import { cardActionAreaClasses, TextField } from "@mui/material";
 import { useNavigate } from "react-router-dom";
-import { setData } from "../plugins/firebase"
+import { setData ,db} from "../plugins/firebase"
+import './Tweet.css'
+import { collection, query, orderBy, onSnapshot } from "firebase/firestore";
+
+
 
 
 
@@ -18,18 +22,65 @@ const [message, setMessage] = useState("")
   
   const handleClick = () => {
     setData(message)
-}
+  }
+
+   const [chat, setChat] = useState<any[]>([])
+  
+useEffect(() =>{
+  const q = query(collection(db, "message"), orderBy("time"));
+  const unsubscribe = onSnapshot(q, (querySnapshot) => {
+    const messagesInfo:any[] = [];
+    querySnapshot.forEach((doc) => {
+      messagesInfo.push(doc.data());
+    });
+    setChat(messagesInfo)
+  });
+    
+  console.log(unsubscribe)
+  return unsubscribe
+
+}, [])
+   
+  
+
+
+  
+  
+  
+  
+  
+  
+  
 
   return (
     <div>
       <h1>Tweet画面</h1>
 
+
+
+    <div className="show-message-area" >
+        {
+          chat.map((chat, index) => {
+            return <h3> key={index} message={chat.message} 
+            </h3>
+          })
+        }
+
+   
+
+      </div>
+
+      
+       
+
+         <div className="sent">
       <TextField
         onChange={e => setMessage(e.target.value)}>
         
        </TextField>
 
-      <button onClick={handleClick}>setData</button>
+        <button onClick={handleClick}>setData</button>
+        </div>
       
 
 
