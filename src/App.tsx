8e98cd4bcd25eaff6a842ead3ca8c2a102f470cb
SatakeYusuke19920527 <satkyusk@icloud.com>
2022-07-17 11:39:42 +0900

    ReduxToolkitでのuser管理を実装

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,39 @@
-import React from 'react';
-import { useNavigate } from "react-router-dom";
+import { useEffect } from 'react';
 import './App.css';
+import { useLoginCheck } from './hooks/useLoginCheck';
 import Login from './pages/Login';
 
-
-
-
 function App() {
-  // const navigate = useNavigate();
+  // const navigate
+  const isLogin = useLoginCheck();
+  useEffect(() => {
+    if (!isLogin) {
+      console.log(`login status : [ ${isLogin} ]`);
+    }
+  }, [isLogin]);
   //  const movePage = (path:string) => {
   //   navigate(`${path}`);
   // }
- 
- 
+
   return (
     <>
-      
-    
       <Login />
-</>
-   
-    
-  
-    
-//  <div>
-//       <h1>login</h1>
-     
-//       <h2
-//         onClick={() => movePage("TkLogin")}
-//        >Takuma's Page</h2>
-//       <hr />
+    </>
 
-//       <h2
-//         onClick={() => movePage("StLogin")}
-//         >Satake's Page</h2>
-    
-//     </div>
-);
+    //  <div>
+    //       <h1>login</h1>
+
+    //       <h2
+    //         onClick={() => movePage("TkLogin")}
+    //        >Takuma's Page</h2>
+    //       <hr />
+
+    //       <h2
+    //         onClick={() => movePage("StLogin")}
+    //         >Satake's Page</h2>
+
+    //     </div>
+  );
 }
 
 export default App;
--- src/app/store.ts
+++ src/app/store.ts
@@ -1,9 +1,9 @@
-import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import { Action, configureStore, ThunkAction } from '@reduxjs/toolkit';
+import userReducer from '../features/user/userSlice';
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
+    user: userReducer,
   },
 });
 
